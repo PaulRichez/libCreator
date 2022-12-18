6a99a757af3c734c373af0335bded4e066f24c92
PaulRichez <paul.richez59@gmail.com>
2022-12-19 00:34:31 +0100

    module cursor root project

---
+++ src/app/cursor/cursor.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CursorComponent } from './cursor.component';
+
+
+
+@NgModule({
+  declarations: [
+    CursorComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CursorModule { }
