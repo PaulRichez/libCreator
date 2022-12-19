cda8e126a3415381c3f40617676cb5df4a74b740
PaulRichez <paul.richez59@gmail.com>
2022-12-19 18:16:02 +0100

    add code happy

--- src/app/modules/cursor/cursor.component.ts
+++ src/app/modules/cursor/cursor.component.ts
@@ -102,4 +102,47 @@ export class CursorComponent {
 `
     }
   ]
+
+  happyCode = [
+    {
+      fileName: 'html',
+      lang: 'html',
+      code: `
+      <div class="flex flex-row around">
+        <div *ngFor="let cursor of cursors" [ngCursor]="'url(' + cursor.url +')'" style="padding:10px; background-color: #5885AF;">
+          <span>{{ cursor.name }}</span>
+        </div>
+      </div>
+      `
+    },
+    {
+      lang: 'ts',
+      fileName: 'ts',
+      code: `
+      import { Component } from '@angular/core';
+
+      @Component({
+        selector: 'app-happy-cursor',
+        templateUrl: './happy-cursor.component.html',
+        styleUrls: ['./happy-cursor.component.scss']
+      })
+      export class HappyCursorComponent {
+        cursors = [
+          {
+            name: 'happy',
+            url: 'assets/cursor/happy.svg',
+          },
+          {
+            name: 'meh',
+            url: 'assets/cursor/meh.svg',
+          },
+          {
+            name: 'sad',
+            url: 'assets/cursor/sad.svg',
+          },
+        ]
+      }
+      `
+    }
+  ]
 }
