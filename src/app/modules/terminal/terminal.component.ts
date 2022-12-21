63526c9f9a2bf5dfd1c53ce0ff521c9a47e783f8
PaulRichez <paul.richez59@gmail.com>
2022-12-21 07:42:16 +0100

    improvement

--- src/app/modules/terminal/terminal.component.ts
+++ src/app/modules/terminal/terminal.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./terminal.component.scss']
 })
 export class TerminalComponent {
+  window = { show: true, title: 'EasyTerminal' };
+  customCommands = [
+    {
+      name: 'askAge',
+      help: 'ask age to user',
+      method: async function (cmd: any) {
+        const age = await cmd.ask('age ?', true);
+        cmd.echo('Age: ' + age);
+      }
+    }
+  ]
 
 }
